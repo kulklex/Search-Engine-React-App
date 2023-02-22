e89cd63fa353bfddd38c753680c9251e8e9d5717
kulklex <kulklex@gmail.com>
2023-02-22 21:13:06 +0100

    Populated Search And Results Components

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useLocation } from 'react-router-dom'
+// import ReactPlayer from 'react-player'
+import { useResultContext } from '../context/ResultContextProvider'
+import { Loading } from './Loading'
 
 export default function Results() {
-  return (
-    <div>Results</div>
-  )
+  const { results, isLoading, getResults, searchTerm } = useResultContext()
+  const location = useLocation() 
+  
+
+  useEffect(() => {
+    if(searchTerm) {
+        if (location.pathname === '/search') {
+            getResults(`search?query=${searchTerm}&num=10&start=0&sort=relevance`)
+        } else  if (location.pathname === '/imagesearch') {
+            getResults(`imagesearch?query=${searchTerm}&num=10&start=0&sort=relevance`)
+        }
+    }
+    getResults('')
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchTerm, location.pathname])
+
+
+  if (isLoading) return <Loading/>
+  
+  switch (location.pathname) {
+    case '/search': 
+        return (
+            <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
+                {results?.items?.map(({link, title}, index) => (
+                <div key={index} className='md:w-2/5 w-full'>
+                    <a href={link} target='_blank' rel='noreferrer'>
+                        <p className='text-sm'>
+                            {link?.length > 30 ? link.substring(0, 30) : link}
+                        </p>
+                        <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                            {title}
+                        </p>
+                    </a>
+                </div>))}
+            </div>
+        )
+
+    case '/imagesearch': 
+        return (<div className='flex flex-wrap justify-center items-center'>
+            {results?.items?.map(({originalImageUrl, title, contextLink}, index) => (
+            <a key={index} className='sm:p-3 p-5' href={contextLink} target='_blank' rel='noreferrer'>
+                <img src={originalImageUrl} alt='' loading='lazy'/>
+                <p className='w-36 break-words text-sm mt-2' >{title}</p>
+
+            </a>))}
+        </div>)
+        
+    // case '/videos':
+    //         return (
+    //           <div className="flex flex-wrap ">
+    //             {results?.results?.map((video, index) => (
+    //               <div key={index} className="p-2">
+    //                 <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px" />
+    //               </div>
+    //             ))}
+    //           </div>
+    //         )     
+        
+    default:
+        return 'ERROR.....'
+  } 
+
+
+  
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { NavLink } from 'react-router-dom'
+import {useDebounce} from 'use-debounce'
+import { useResultContext } from '../context/ResultContextProvider'
 
 export default function Search() {
+  const [text, setText] = useState('Google Search Api')
+  const {setSearchTerm} = useResultContext()
+ 
+
+  // On complicated projects where you have an input field, if you want some changes to happen whenever the user types something in
+  // By default as the user is typing each individual letter, a new request is being made. We do not want that, we're going to DEBOUNCE the input
+  const {debouncedValue} = useDebounce(text, 500) // we're deBouncing it every 500 milliseconds
+
+
+  useEffect(() => {
+    if(debouncedValue) setSearchTerm(debouncedValue)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue])
+
   return (
-    <div>Search</div>
+    <div className='relative lg:ml-48 md:ml-72 lg:mt-5 mt-3'>
+      
+      <input value={text} type="text" className='lg:w-96 w-60 h-10 dark:bg-gray-200 lg:ml-0 md:ml-8 ml-24 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg'  
+        placeholder="🔎 Search or type URL" onChange={(e) => setText(e.target.value)}/>
+      
+      {text && (<button type='button' onClick={() => setText('')} className='absolute top-1.5 right-4 text-2xl text-gray-500 '>
+        X
+      </button>)}
+
+      <div className="flex justify-around items-center mt-4">
+        <NavLink to='/search'  className="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2  ">🔎 All</NavLink>
+        <NavLink to='/imagesearch' className="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2 ">📸 Images</NavLink>
+      </div>
+
+    </div>
   )
 }
